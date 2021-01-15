e1fe4e1938ce4cd96e2cbde469e8af75203f645d
An Nguyễn <nguyenvanan.12a8@gmail.com>
2021-01-14 16:54:49 -0800

    feat: Finish stack

--- golang/stackandqueue/stack.go
+++ golang/stackandqueue/stack.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 )
 
 const maxSize int = 100
@@ -16,21 +17,61 @@ func (s *Stack) init(){
 }
 
 // push a key into stack
-func (s *Stack) push(key int)  {
+func (s *Stack) push(key int) error {
 	if s.top > maxSize - 1 {
-		errors.New("Stack overflow")
-		return
+		return errors.New("Stack overflow")
 	}
 	s.top++
 	s.data[s.top] = key
+	return nil
 }
 
-func (s *Stack) pop() int {
+func (s *Stack) pop() (int, error) {
 	if s.top < 0 {
-		errors.New("Stack underflow")
-		return 0
+		err := errors.New("Stack underflow")
+		return 0, err
 	}
 	x := s.data[s.top]
 	s.top--
-	return x
+	return x, nil
+}
+
+func (s *Stack) isEmpty() bool {
+	if s.top < 0 {
+		return true
+	}
+	return false
+}
+
+func (s *Stack) isFull() bool {
+	if s.top >= maxSize - 1 {
+		return true
+	}
+	return false
+}
+
+func (s *Stack) peek(idx int) (int, error) {
+	if idx > s.top {
+		err := errors.New("Not found")
+		return 0, err
+	}
+	return s.data[idx], nil
+} 
+
+func (s *Stack) count() int {
+	if s.top < 0 {
+		return 0
+	}
+	return s.top + 1
+}
+
+func (s *Stack) change(idx, val int) {
+	s.data[idx] = val
+}
+
+func (s *Stack) display() {
+	for i := len(s.data) - 1; i >= 0; i-- {
+		fmt.Print(s.data[i], " ")
+	}
+	fmt.Println()
 }
--- golang/stackandqueue/stack_test.go
+++ golang/stackandqueue/stack_test.go
@@ -35,8 +35,71 @@ func TestPop(t *testing.T) {
 	stack.push(3)
 
 	for _, val := range []int{3,2,1} {
-		if out := stack.pop(); out != val {
+		if out, _ := stack.pop(); out != val {
 			t.Errorf("Stack.pop() != %d", val)
 		}
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	var stack Stack
+	stack.init()
+	stack.push(1)
+
+	if stack.isEmpty() {
+		t.Errorf("Stack.isEmpty() != %v", false)
+	}
+	stack.pop()
+	if !stack.isEmpty() {
+		t.Errorf("Stack.isEmpty() != %v", true)
+	}
+}
+
+func TestIsFull(t *testing.T) {
+	var stack Stack
+	stack.init()
+	if stack.isFull() {
+		t.Errorf("Stack.isEmpty() != %v", false)
+	}
+	for i := 0; i < 100; i++ {
+		stack.push(i)
+	}
+	if !stack.isFull() {
+		t.Errorf("Stack.isEmpty() != %v", true)
+	}
+}
+
+func TestPeek(t *testing.T) {
+	var stack Stack
+	stack.init()
+	stack.push(1)
+	stack.push(2)
+	stack.push(3)
+
+	p, _ := stack.peek(1)
+	if p != 2 {
+		t.Errorf("Stack.peek(%d) != %d", 1, 2)
+	}
+}
+
+func TestCount(t *testing.T) {
+	var stack Stack
+	stack.init()
+	stack.push(1)
+	count := stack.count()
+	if count != 1 {
+		t.Errorf("Stack.count() != %d", 1)
+	}
+}
+
+func TestChange(t *testing.T) {
+	var stack Stack
+	stack.init()
+	stack.push(1)
+	stack.push(2)
+	stack.push(3)
+	stack.change(2, 5)
+	if out, _ := stack.peek(2); out != 5 {
+		t.Errorf("Stack.change(%d, %d) != %d", 2, 5, 5)
 	}
 }
